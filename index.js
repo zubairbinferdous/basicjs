d7fb9307d70b8f0f0d8c847a3721b508e5289378
zubair bin ferdous <zubairbinf@gmail.com>
2021-01-19 05:28:48 +0600

    js first

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('jankar mahabub js class');
+
+var BananaPrice = 12;
+console.log(BananaPrice);
+
+var Age = 22;
+console.log(typeof Age);
+
+var Name ="zubair bin ferdous";
+console.log(typeof Name);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+
+var FirstName="zubair";
+var LastName="bin ferdous";
